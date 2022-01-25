7263241bf0092b33f7973f2b13be27a834eb8105
Miguel Hernández Giusti <mhernandezve@users.noreply.github.com>
2022-01-25 17:18:47 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,7 +10,7 @@ async def test_read_root():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {"msg": "Hello Work"}
 
 
 @pytest.mark.asyncio
